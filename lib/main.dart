b81ff5897871c2f8193090762e639857424e7d60
Dhiren Upadhyay <dhirenupadhyay@apples-MacBook-Pro.local>
2022-10-10 21:59:34 -0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:user_app/screens/register.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterScreen(),
+    );
+  }
+}
+
